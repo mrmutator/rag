ed2f12517719d14961b2e22adaf7123e00ae91fa
Roger Wechsler <mr.mutator@gmail.com>
2023-10-20 15:33:21 +0200

    Moved config from yaml to env vars

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# rag
+# RAG - Retrieval Augmented Generation
+
+This is a Streamlit application that allows setting up and testing a RAG application with Azure Cognitive Search and Azure Open AI.
+
+## Setup
+TBD
+
+## Running the application
+
+Run the application with streamlit using the following command:
+
+```python -m streamlit run app.py```
+
+The following environment variables need to be defined:
+
+| Variable name                      | Description                                          |
+|------------------------------------|------------------------------------------------------|
+| AZURE_COGNITIVE_SEARCH_ENDPOINT    | Endpoint of the Azure Cognitive Search service       |
+| AZURE_COGNITIVE_SEARCH_INDEX_NAME  | Name of the Azure Cognitive Search index             |
+| AZURE_COGNITIVE_SEARCH_KEY         | Search key of the Azure Cognitive Search service     |
+| AZURE_OPEN_AI_ENDPOINT             | Endpoint of the Azure Open AI service                |
+| AZURE_OPEN_AI_KEY                  | API key of the Azure Open AI service                 |
+| AZURE_OPEN_AI_EMBEDDING_DEPLOYMENT | Name of the Azure Open AI embedding model deployment |
+| AZURE_OPEN_AI_CHAT_DEPLOYMENT      | Name of the Azure Open AI Chat model deployment      |
--- src/rag/utils/config.py
+++ src/rag/utils/config.py
@@ -1,45 +1,51 @@
 """Config module that provides helper functions to set up the app"""
 
-from typing import Dict, Union
+import os
+from typing import Dict
 
-import yaml
-from yaml.loader import SafeLoader
+from dotenv import load_dotenv
 
 from ..core.chatbot import Chatbot
 from ..core.llm import LLM, configure_openapi
 from ..core.search import SearchService
 
-CHAT_DEPLOYMENT = "chat_deployment"
-EMBEDDING_DEPLOYMENT = "embedding_deployment"
-ENDPOINT = "endpoint"
-HYBRID_SEARCH = "hybrid_search"
-KEY = "key"
-OPEN_AI = "openai"
-SEARCH = "search"
-VECTOR_FIELDS = "vector_fields"
+AZURE_COGNITIVE_SEARCH_ENDPOINT = "AZURE_COGNITIVE_SEARCH_ENDPOINT"
+AZURE_COGNITIVE_SEARCH_INDEX_NAME = "AZURE_COGNITIVE_SEARCH_INDEX_NAME"
+AZURE_COGNITIVE_SEARCH_KEY = "AZURE_COGNITIVE_SEARCH_KEY"
+AZURE_OPEN_AI_ENDPOINT = "AZURE_OPEN_AI_ENDPOINT"
+AZURE_OPEN_AI_KEY = "AZURE_OPEN_AI_KEY"
+AZURE_OPEN_AI_EMBEDDING_DEPLOYMENT = "AZURE_OPEN_AI_EMBEDDING_DEPLOYMENT"
+AZURE_OPEN_AI_CHAT_DEPLOYMENT = "AZURE_OPEN_AI_CHAT_DEPLOYMENT"
+
+REQUIRED_ENV_VARIABLES = [AZURE_COGNITIVE_SEARCH_ENDPOINT,
+                          AZURE_COGNITIVE_SEARCH_INDEX_NAME,
+                          AZURE_COGNITIVE_SEARCH_KEY,
+                          AZURE_OPEN_AI_ENDPOINT,
+                          AZURE_OPEN_AI_KEY,
+                          AZURE_OPEN_AI_EMBEDDING_DEPLOYMENT,
+                          AZURE_OPEN_AI_CHAT_DEPLOYMENT]
 
 
-def load_config(file_path: str = "config.yaml") -> dict:
-    with open(file_path) as f:
-        return yaml.load(f, Loader=SafeLoader)
+def load_config() -> Dict[str, str]:
+    load_dotenv()
+    config = dict()
+    for var_name in REQUIRED_ENV_VARIABLES:
+        var_value = os.getenv(var_name)
+        if var_value is None:
+            raise ValueError(f"Environment variable not defined: {var_name}")
+        config[var_name] = var_value
+    return config
 
 
-def to_yaml(config: Dict[str, str]):
-    return yaml.dump(config)
+def create_chatbot(config: Dict[str, str]) -> Chatbot:
+    configure_openapi(endpoint=config[AZURE_OPEN_AI_ENDPOINT], key=config[AZURE_OPEN_AI_KEY])
 
+    llm = LLM(chat_deployment_name=config[AZURE_OPEN_AI_CHAT_DEPLOYMENT],
+              embedding_deployment_name=config[AZURE_OPEN_AI_EMBEDDING_DEPLOYMENT])
+    search = SearchService(endpoint=config[AZURE_COGNITIVE_SEARCH_ENDPOINT],
+                           search_key=config[AZURE_COGNITIVE_SEARCH_KEY],
+                           index_name=config[AZURE_COGNITIVE_SEARCH_INDEX_NAME],
+                           llm=llm)
 
-def from_yaml(s: str):
-    return yaml.safe_load(s)
-
-
-def create_chatbot(config: Dict[str, Union[str, Dict[str, str]]]) -> Chatbot:
-    openai_config = config[OPEN_AI]
-    search_config = config[SEARCH]
-
-    configure_openapi(openai_config[ENDPOINT], openai_config[KEY])
-
-    llm = LLM(openai_config[CHAT_DEPLOYMENT], openai_config[EMBEDDING_DEPLOYMENT])
-    search = SearchService(llm=llm, **search_config)
-
-    chatbot = Chatbot(llm, search)
+    chatbot = Chatbot(llm=llm, search=search)
     return chatbot
